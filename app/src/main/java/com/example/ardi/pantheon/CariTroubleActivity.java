8f96fdb192b74e7f982a42cf5f282f169b6ce74f
ardisptraaa <exc.ardisaputra@gmail.com>
2017-11-02 12:04:20 +0700

    layout splash dan layout menu sudah dibuat. selanjutnya berikan image view di dlm image button dan teks pada layout menu

---
+++ app/src/main/java/com/example/ardi/pantheon/CariTroubleActivity.java
@@ -0,0 +1,13 @@
+package com.example.ardi.pantheon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CariTroubleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cari_trouble);
+    }
+}
