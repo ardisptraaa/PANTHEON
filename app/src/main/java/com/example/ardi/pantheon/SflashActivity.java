4a3910645112e11edaa886979cc43f5ab48fba21
AyasZakaria <ayaszakaria1805@gmail.com>
2017-11-06 12:04:47 +0700

    Custom Listview, Menu OS, Linux, Mac Dan WIndows

--- app/src/main/java/com/example/ardi/pantheon/SflashActivity.java
+++ app/src/main/java/com/example/ardi/pantheon/SflashActivity.java
@@ -12,6 +12,5 @@ public class SflashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sflash);
-        //
     }
 }
